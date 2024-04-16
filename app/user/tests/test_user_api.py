d967ef8b273c635360489b3f6d4a538998230d7d
Dana S <dana.s.lewis@gmail.com>
2024-04-16 18:44:12 -0400

    implemented tests for manage user API 2X.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -13,6 +13,7 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
@@ -100,7 +101,6 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
 
     def test_retrieve_user_unauthorized(self):
         """Test authentication is required for users."""
